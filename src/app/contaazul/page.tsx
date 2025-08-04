aa506f7b1b862f3f60739bc203f0450d35019305
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-08-04 11:41:17 -0300

    Troca da url

--- src/app/contaazul/page.tsx
+++ src/app/contaazul/page.tsx
@@ -4,10 +4,9 @@ export default function ContaAzul() {
 	function handleRedirectToContaAzul() {
 		const clientId = process.env.NEXT_PUBLIC_CONTAAZUL_CLIENT_ID!;
 		const redirectUri = process.env.NEXT_PUBLIC_CONTAAZUL_REDIRECT_URI!;
-		const scope = 'sales';
 
-		const authUrl = `https://api.contaazul.com/auth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`;
-
+		const authUrl = `https://auth.contaazul.com/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=ESTADO&scope=openid+profile+aws.cognito.signin.user.admin`
+		
 		window.location.href = authUrl;
 	}
 
