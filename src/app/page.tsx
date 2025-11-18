092a06d189e4c1fa2b49c94fa08f6513a438baab
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-11-18 16:34:01 -0300

    Removendo imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { ItemComponent } from './_components/ItemComponent';
 import { ItemProps } from '@/types/Item';
 
 export default function Home() {
