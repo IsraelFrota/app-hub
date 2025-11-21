c9f4de362e65a9365274b56c77e13254707682f6
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-11-21 11:53:40 -0300

    Ordem de apresentação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import { ItemProps } from '@/types/Item';
 export default function Home() {
   const apps: ItemProps[] = [
     { title: 'App 5s', icon: '📋', url: 'http://192.168.0.18:3001/', description: 'System for performing 5S methodology audits.'},
+    { title: 'Audit criteria', icon: '📖', url: 'https://docs.google.com/spreadsheets/d/10YdvT6qfqdJuHmHZp_KXCZ_8DV_h6C1bkgpsDwVKqsY/edit?usp=sharing', description: 'Spreadsheet with evaluation criteria for the 5S audit.' }
     { title: 'Dashboard 5s', icon: '📊', url: 'http://192.168.0.18:3001/ui/dashboard', description: 'Dashboard for visualizing 5S audit results.' },
     { title: 'IF Music', icon: '🎵', url: 'http://192.168.0.18:9078/', description: 'Local music player for on-site streaming.' },
     { title: 'File server', icon: '🗄️', url: 'http://192.168.0.99:8081/', description: 'Local server for managing and accessing shared files.' },
     { title: 'Book catalog', icon: '📚', url: 'https://docs.google.com/spreadsheets/d/1Qd5tLyTvan9-EJuW2g_UYQaV7HRADrMuA402mDRNo2o/edit?gid=921363456#gid=921363456', description: 'Google spreadsheet with the available book catalog.' },
     { title: 'Snack times', icon: '🍔', url: 'https://docs.google.com/spreadsheets/d/1Ti7rzzUv6jqkb_9ih_zK3D9nyJwS8kPiTagHesTGHq4/edit?gid=0#gid=0', description: 'Google spreadsheet containing the snack schedule.' },
-    { title: 'Audit criteria', icon: '📖', url: 'https://docs.google.com/spreadsheets/d/10YdvT6qfqdJuHmHZp_KXCZ_8DV_h6C1bkgpsDwVKqsY/edit?usp=sharing', description: 'Spreadsheet with evaluation criteria for the 5S audit.' }
   ];
 
   const [search, setSearch] = useState('');
