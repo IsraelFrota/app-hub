c051a15d515d24dab8d6f24e055318f09354def1
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-01-13 16:05:14 -0300

    Ajuste na arquitetura de gerenciar as conexões com o mongo

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,52 +1,63 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 interface MongooseCache {
-  conn: typeof import('mongoose') | null;
-  promise: Promise<typeof import('mongoose')> | null;
+  conn: typeof import("mongoose") | null;
+  promise: Promise<typeof import("mongoose")> | null;
 }
 
 declare global {
-  var mongoose: MongooseCache | undefined;
+  var mongooseMain: MongooseCache | undefined;
+  var mongooseV2: MongooseCache | undefined;
 }
 
-const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+const cachedMain: MongooseCache = global.mongooseMain || {
+  conn: null,
+  promise: null,
+};
+
+const cachedV2: MongooseCache = global.mongooseV2 || {
+  conn: null,
+  promise: null,
+};
 
 export async function connectToDatabase() {
-  const MONGODB_URI = process.env.MONGODB_URI!;
-  
-  if (!MONGODB_URI) {
-    throw new Error('MONGODB_URI não definida nas variáveis de ambiente');
-  }
-  
-  if (cached.conn) return cached.conn;
+  const MONGODB_URI = process.env.MONGODB_URI;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI não definida nas variáveis de ambiente");
+  }
+
+  if (cachedMain.conn) return cachedMain.conn;
+
+  if (!cachedMain.promise) {
+    cachedMain.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
     });
   }
 
-  cached.conn = await cached.promise;
-  global.mongoose = cached;
-  return cached.conn;
+  cachedMain.conn = await cachedMain.promise;
+  global.mongooseMain = cachedMain;
+
+  return cachedMain.conn;
 }
 
 export async function connectToDatabaseV2() {
-  const MONGODB_URI_V2 = process.env.MONGODB_URI_V2!;
+  const MONGODB_URI_V2 = process.env.MONGODB_URI_V2;
 
   if (!MONGODB_URI_V2) {
-    throw new Error('MONGODB_URI_V2 não definida nas variáveis de ambiente');
+    throw new Error("MONGODB_URI_V2 não definida nas variáveis de ambiente");
   }
 
-  if (cached.conn) return cached.conn;
+  if (cachedV2.conn) return cachedV2.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI_V2, {
+  if (!cachedV2.promise) {
+    cachedV2.promise = mongoose.connect(MONGODB_URI_V2, {
       bufferCommands: false,
     });
   }
 
-  cached.conn = await cached.promise;
-  global.mongoose = cached;
-  return cached.conn;
+  cachedV2.conn = await cachedV2.promise;
+  global.mongooseV2 = cachedV2;
+
+  return cachedV2.conn;
 }
