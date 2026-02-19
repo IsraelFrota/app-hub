5f0b326477640695250a15540da10db5af136a82
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-02-19 15:29:39 -0300

    refactor: Dividindo a reponsabilidade do formulário de login

---
+++ src/app/_components/auth/LoginFormContainer.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { toast } from "sonner";
+
+import { LoginFormView } from "./LoginFormView";
+import { loginAction } from "@/app/_server/action";
+
+import {
+	loginSchema,
+	type LoginForm,
+} from "@/schema/loginSchema";
+
+export function LoginFormContainer() {
+	const [loading, setLoading] = useState(false);
+
+	const loginForm = useForm<LoginForm>({
+		resolver: zodResolver(loginSchema),
+		mode: "onChange",
+		defaultValues: {
+			email: "",
+			password: "",
+		}
+	});
+
+	async function onSubmit(values: LoginForm) {
+		setLoading(true);
+		try {
+			const response = await fetch("/api/user", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: values.email,
+					password: values.password
+				}),
+			});
+
+			if (!response.ok) {
+				toast.error("Error ao logar");
+				return;
+			}
+
+			toast.success("Login realizado com sucesso!");
+			loginAction(values);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+
+	return (
+		<LoginFormView
+			loading={loading}
+			onSubmit={onSubmit}
+			loginForm={loginForm}
+		/>
+	);
+}
---
+++ src/app/_components/auth/LoginFormView.tsx
@@ -0,0 +1,73 @@
+import { useForm } from "react-hook-form";
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "@/components/ui/form";
+
+import { type LoginForm } from "@/schema/loginSchema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+type LoginFormProps = {
+	loginForm: ReturnType<typeof useForm<LoginForm>>;
+	onSubmit: (value: LoginForm) => Promise<void>;
+	loading: boolean;
+};
+
+export function LoginFormView({
+	loading,	
+	onSubmit,
+	loginForm,
+}: LoginFormProps) {
+	return (
+		<fieldset disabled={loading}>
+			<Form { ...loginForm }>
+				<form
+					className="space-y-3"
+					onSubmit={loginForm.handleSubmit(onSubmit)}
+				>
+					<div className="flex flex-col space-y-2">
+						<FormField
+							control={loginForm.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>E-mail</FormLabel>
+									<FormControl>
+										<Input
+											{ ...field }
+											type="email"
+											placeholder="example@email.com.br"
+										/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={loginForm.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Senha</FormLabel>
+									<FormControl>
+										<Input
+											{ ...field }
+											type="password"
+											placeholder="**********"
+										/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					</div>
+					<Button type="submit" disabled={loading}>Entrar</Button>
+				</form>
+			</Form>
+		</fieldset>
+	);
+}
