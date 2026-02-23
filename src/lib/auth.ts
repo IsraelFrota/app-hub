3de6eb57f76dc07facd6a52376cda08a2dd42608
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-02-23 14:49:29 -0300

    refactor: Eliminando uma rota de autenticação com um helper central

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export async function isAuthenticated() {
+  const cookieStore = await cookies();
+
+  const session = cookieStore.get('session');
+
+  return session?.value === process.env.TOKEN;
+}
