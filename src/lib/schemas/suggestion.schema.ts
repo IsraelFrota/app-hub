d8f1af7306eb2451362b611bf2cf29481430fd2c
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-02-23 14:39:57 -0300

    refactor: Centralizando os schemas

---
+++ src/lib/schemas/suggestion.schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const suggestionSchema = z.object({
+	name: z
+		.string()
+		.toLowerCase()
+		.optional(),
+	text: z
+		.string()
+		.min(3, 'Insira uma mensagem significativa'),
+	type: z
+		.enum(['suggestion', 'feedback']),
+	vote: z
+		.number(),
+});
+
+export type SuggestionForm = z.infer<typeof suggestionSchema>;
